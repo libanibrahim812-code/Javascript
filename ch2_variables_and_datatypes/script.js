ac04fec6a382e3b137f03281d0fe2241a66375b1
Liban Ibrahim Ali <libanibrahim812@gmail.com>
2026-03-16 16:55:23 +0300

    i learnt here javascript variables & datatypes

--- ch2_variables_and_datatypes/script.js
+++ ch2_variables_and_datatypes/script.js
@@ -2,6 +2,7 @@ var firstName = "Liban";
 var lasttName = "Ali";
 var height = 1.83;
 var isMarried = false;
+
 console.log(firstName);
 console.log(lasttName);
 console.log(height);
@@ -10,7 +11,7 @@ console.log(isMarried);
 //kawran hadaa dhaho si aan isugu dayo in confuse ku furo computerka
 // console.log(firstName * lasttName);
 // Nan maxa laga wadaa not a number yaa ogesiyay computer
-// wuxuu ku ogaday wuxu yaqaanaa data type inee tahay text and this operation is not suitable 
+// wuxuu ku ogaday wuxu yaqaanaa data type inee tahay text and this operation is not suitable
 // console.log(height.toUppercase()); also not possible
 // every data type has its own operation
 // console.log(height * 5); // this will worked
@@ -18,7 +19,6 @@ console.log(isMarried);
 // *******Knowing your data type************
 // console.log(typeof height);
 // console.log(typeof firstName);
-
 
 //******* declaring and assigning terminologies ********/
 // var city; //this means only declaring
@@ -30,7 +30,6 @@ console.log(isMarried);
 // var x;
 // console.log(x);
 
-//null is place hlder if you find your suitable data put it there
+//null is place hlder if you find your suitable data put it there.
 // var y = "null";
 // console.log(y);
-
