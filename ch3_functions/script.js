9d90dbb7784f7f74553279e85f3bd1fc73dd27c5
Liban Ibrahim Ali <libanibrahim812@gmail.com>
2026-03-16 19:53:37 +0300

    i learnt here functions

---
+++ ch3_functions/script.js
@@ -0,0 +1,62 @@
+// console.log("video games");
+// console.log("action movies");
+// console.log("challenges");
+// console.log("online gaming");
+// console.log("cute animals");
+// console.log("romantics");
+// console.log("cute outfis");
+// console.log("makeUp");
+
+// *****My boss came and he told me i must print what the boys like while i did not remove and commnet what girls *****
+
+// function whatTheBoysLike() {
+//    console.log("video games");
+//    console.log("action movies");
+//    console.log("challenges");
+//    console.log("online gaming");
+// }
+
+// whatTheBoysLike();
+
+// function whatGirlsLike(){
+//    console.log("cute animals");
+//    console.log("romantics");
+//    console.log("cute outfis");
+//    console.log("makeUp");
+// }
+// whatGirlsLike()
+
+
+
+// Parametres and Returning Data
+// function colors(){
+//    console.log(data1);
+//    console.log(data2);
+// }
+// colors()
+// the output will be data is not defined
+
+//we pass data to the function
+// function colors(data1, data2) {
+//   console.log(data1);
+//   console.log(data2);
+// }
+// colors();
+// the output will be data is not defined
+
+// function colors(data1, data2) {
+//   console.log(data1);
+//   console.log(data2);
+// }
+
+// colors("red","blue"); // this arguments
+
+// function multiply (x, y){
+//     return  x * y;
+// }
+// // console.log(multiply(7, 7));
+
+// // OR
+
+// var total = multiply(7, 7);
+// console.log(total);
