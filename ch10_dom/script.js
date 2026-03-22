12b4823d0cc90b50502785fcc9e5f369f4b64512
Liban Ibrahim Ali <libanibrahim812@gmail.com>
2026-03-22 11:11:36 +0300

    i learnt here Dom basics

---
+++ ch10_dom/script.js
@@ -0,0 +1,49 @@
+// let bigTitle = document.createElement("h1");
+// bigTitle.textContent = "Liban"
+// document.body.appendChild(bigTitle)
+//in the html body add this one is the meaning this line
+
+// *******what about if i want more so as to repeat we use loop*********
+
+// for (i = 0; i < 10; i++) {
+//   let bigTitle = document.createElement("h1");
+//   bigTitle.textContent = i + " " + "Liban";
+//   bigTitle.style.color = "red";
+//   bigTitle.style.backgroundColor = "cyan";
+//   document.body.appendChild(bigTitle);
+// }
+
+//*********Get element by id Namw**********/
+
+// let p = document.getElementById("mow");
+// // p.textContent = "we changed this"
+// p.style.color = "blue";
+// p.style.fontSize = "60px";
+
+//*********Get element by Tag Namw**********/
+
+// let allParagraphs = document.getElementsByTagName("p");
+// for (let i = 0; i < allParagraphs.length; i++) {
+//     allParagraphs[i].style.color = "blue";
+    
+// }
+
+//*********Get element by Namw**********/
+
+// let  allParagraphs = document.getElementsByName("pMember");
+// for (let i = 0; i < allParagraphs.length; i++) {
+//   allParagraphs[i].style.color = "blue";
+//   allParagraphs[i].style.borderStyle = "solid"
+//   allParagraphs[i].style.borderColor = "orange"
+//   allParagraphs[i].style.fontSize = "45px"
+// }
+
+//*********Get element by class Namw**********/
+
+// let allClass = document.getElementsByClassName("blueBorder")
+// for(i = 0; i < allClass.length; i++ ){
+// allClass[i].style.borderStyle = "solid"
+// allClass[i].style.color = "blue"
+// }
+
+
---
+++ ch10_dom/script2.js
@@ -0,0 +1,89 @@
+// createElemen and appendChild
+
+// let bigTitle = document.createElement("h1");
+// bigTitle.textContent = "Liban";
+// document.body.appendChild(bigTitle);
+
+// my boss told me 100 times h1
+// for (i = 0; i < 100; i++){
+//     let bigTitle = document.createElement("h1")
+//     bigTitle.textContent = i+" "+"Liban"
+//     document.body.appendChild(bigTitle)
+
+// }
+
+//-----------------------------------------------------
+
+//insertBefore
+// let spanElement = document.createElement("span")
+// spanElement.textContent = "Before element"
+// document.body.insertBefore(spanElement,bigTitle)
+
+// //-----------------------------------------------------
+
+// //replaceChild
+// let divElement = document.createElement("div")
+// divElement.textContent = "div ugu horeeye"
+// document.body.replaceChild(divElement,spanElement)
+
+// //-----------------------------------------------------
+
+// //removeChild
+// document.body.removeChild(divElement);
+
+//innerHtml : wuxuu aqoonsanaa tagska
+// let container = document.createElement("div");
+// container.innerHTML = "<p>paragraph1 inside div</p>";
+// document.body.appendChild(container);
+
+//***********************************************************************************************
+
+// Selecting HTML elements and CSS selectors with JavaScript DOM, so as to modify control it.
+
+//getElementById
+// let greeting = document.getElementById("mow");
+// greeting.textContent = "Karibu"
+
+//querySelector() is for single like id
+
+// let greeting = document.querySelector("#mow");
+// greeting.textContent = "Karibu"
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//getElementsByTagName
+// let allTheH1s = document.getElementsByTagName("h1");
+// for (i = 0; i < allTheH1s.length; i++) {
+//   allTheH1s[i].style.color = "red";
+// }
+
+// let allTheH1s = document.querySelectorAll("h1");
+// for (i = 0; i < allTheH1s.length; i++) {
+//   allTheH1s[i].style.color = "red";
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//getElementByName
+//  for(let i = 0; i < allTheH1s.length; i++){
+// allTheH1s[i].style.color = "blue"
+//  }
+
+// let allTheH1s = document.querySelectorAll("[name = h1Member]");
+// for (let i = 0; i < allTheH1s.length; i++) {
+// allTheH1s[i].style.color = "blue";
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//getElementsByClassName
+// let parag = document.getElementsByClassName("blueBorder");
+// for (let i = 0; i < parag.length; i++) {
+// parag[i].style.color = "skyblue";
+// }
+
+//querySelectorall() is for more than one
+// let parag = document.querySelectorAll(".blueBorder");
+// for (let i = 0; i < parag.length; i++) {
+// parag[i].style.color = "blueviolet"
+// }
